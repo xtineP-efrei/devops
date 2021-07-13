318c71c5b8ef51ee253d15196b5f92ab498d7d63
Christine Penas <christine.penas@efrei.net>
2021-07-13 16:58:28 +0200

    stop container before run

--- update_app.sh
+++ update_app.sh
@@ -1,3 +1,4 @@
 git pull https://github.com/xtineP-efrei/devops.git
 docker build . -t christine
+docker stop $(docker ps -q --filter ancestor=christine )
 docker run --rm -d -p 3999:3999 christine  nodejs app.js
