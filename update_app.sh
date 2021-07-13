7a3d266a02b5be56d04c1220261dfa78ac281283
Christine Penas <christine.penas@efrei.net>
2021-07-13 17:16:06 +0200

    correction pull git in update_app.sh

--- update_app.sh
+++ update_app.sh
@@ -1,4 +1,4 @@
-git pull https://github.com/xtineP-efrei/devops.git
+git pull origin main
 docker build . -t christine
 docker stop $(docker ps -q --filter ancestor=christine )
 docker run --rm -d -p 3999:3999 christine  nodejs app.js
